ea8e17f0099e845ee4d270ab216d55f6ac6b3396
manan gandhi <gandhimanan1@gmail.com>
2023-06-27 14:44:10 -0400

    restructured

--- my-portfolio/projects.py
+++ my-portfolio/projects.py
@@ -3,6 +3,8 @@ projects=[
      },
          {'2':{'title':"Solar Educational Application",'Description':"Android application made using Flutter","Tools":"Flutter, Dart",'img':"../static/images/solarpanel.png"}
      },
-         {'3':{'title':"Raspberrypi WebServer",'Description':"Designed a webserver with custom Mods and Firebase","Tools":"Flask, Python, HTML, CSS",'img':"../static/images/piserver.png"}
+       {'3':{'title':"Second Handz",'Description':"A complete ecommerce app that will help you buy and sell stuff.","Tools":"Flask, Python, HTML, CSS",'img':"../static/images/ecommerce.png"}
+     },
+         {'4':{'title':"Raspberrypi WebServer",'Description':"Designed a webserver with custom Mods and Firebase","Tools":"Flask, Python, HTML, CSS",'img':"../static/images/piserver.png"}
      },
 ]
